4caa74d56f9424928886711397451f68f77413da
Cameron Johnson <cameronjohnson@camerons-air-2.lan>
2024-07-14 14:09:37 -0600

    went back

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,20 @@
 import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter({
-			fallback: '404.html'
-		}),
-		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		}
-	}
+  preprocess: vitePreprocess(),
+
+  kit: {
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: '404.html'
+    }),
+    paths: {
+      base: process.env.NODE_ENV === 'development' ? '' : '/Cameron-Website'
+    }
+  }
 };
 
 export default config;
