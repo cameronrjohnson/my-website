70c1e5930cd1e0ebac91c9ce37881cc7955aacf1
Cameron Johnson <cameronjohnson@camerons-air-2.lan>
2024-07-14 14:03:26 -0600

    went back

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,5 @@
-<nav>
-	<a href="/">home</a>
-	<a href="/about">about</a>
-</nav>
-
-<h1>home</h1>
-<p>this is the home page.</p>
+<h1>Welcome to SvelteKit, this is new!</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<body>
+    This works
+</body>
