5042a3b053950d60cd20a8f1d41c4617401011ad
Cameron Johnson <cameronjohnson@camerons-air-2.lan>
2024-07-15 11:22:02 -0600

    Added the nav

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+    import { base } from '$app/paths';
+</script>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Personal Dev Website</title>
+</head>
+<body>
+    <header>
+        <div class="container">
+            <h1>Cameron Johnson</h1>
+            <nav>
+                <ul>
+                    <li><a href="{base}/">Home</a></li>
+                    <li><a href="{base}/">Portfolio</a></li>
+                    <li><a href="{base}/">Resume</a></li>
+                    <li><a href="{base}/">Contact Me</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+</body>
+
+<slot />
+
+<footer>
+    <div class="container">
+        <p>&copy; 2024 Cameron Johnson. All Rights Reserved.</p>
+        <div class="social-media">
+            <a href="https://github.com/CameronRJohnson" target="_blank">GitHub</a>
+            <a href="https://www.linkedin.com/in/cameron-johnson-b13b54200/" target="_blank">LinkedIn</a>
+            <a href="https://www.instagram.com/cameron.__.j/?hl=en" target="_blank">Instagram</a>
+        </div>
+    </div>
+</footer>
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,2 @@
-<script lang="ts">
-    import { base } from '$app/paths';
-</script>
-
-
-<nav>
-	<a href="{base}/">home</a>
-	<a href="{base}/about">about</a>
-</nav>
-
 <h1>home</h1>
-<p>this is the home page.</p>
+<p>this is the home page test.</p>
