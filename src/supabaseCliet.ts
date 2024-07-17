f08f91a907724596a237ad73783d90a4ced48142
Cameron Johnson <cameronjohnson@camerons-air-2.lan>
2024-07-17 02:13:54 -0600

    Added the contact page

--- src/supabaseCliet.ts
+++ src/supabaseCliet.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SVELTE_APP_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SVELTE_APP_SUPABASE_ANON_KEY;
+const supabaseUrl = "https://ukklxojprfpvtwiknvtj.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVra2x4b2pwcmZwdnR3aWtudnRqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjExNjA2OTksImV4cCI6MjAzNjczNjY5OX0.IzfHPI2GZ1fVbY5QoJgxyvHOQ4B_4WZvjEkuwS66akw";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
